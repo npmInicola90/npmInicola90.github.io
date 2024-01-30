7073924888809313c585bfaca17f5e177d90bee4
Nicola leone <20nleone@conyers.org.uk>
2024-01-30 19:11:26 +0000

    ghjghj

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '51fd1b13c3b6ac577573647ab8c9bffbd2569ca5',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '51fd1b13c3b6ac577573647ab8c9bffbd2569ca5',
+    ),
+    'appsero/client' => 
+    array (
+      'pretty_version' => 'v1.1.11',
+      'version' => '1.1.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b313bf878a010a0892496ab3844ec3dddc5a07ac',
+    ),
+  ),
+);
